da2bbaa3f8a5133919c5f778d6ead2552a095943
aqua <aqua.rock.slug@gmail.com>
2026-09-23 22:37:07 -0500

    destructuring littlejs variable bug

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import favicon from "../assets/favicon.png";
 import tilesURL from "../assets/tiles.png";
 
 // biome-ignore format: un-prefix frequently used littlejs functions
-const { vec2, vec3, tile, hsl, rand, PI, sin, time, drawTile, drawText, render3D } = l;
+const { vec2, vec3, tile, hsl, rand, PI, sin, drawTile, drawText } = l;
 
 // reload on rebuild when developing
-if (!PRODUCTION)
-	new EventSource("/esbuild").addEventListener("change", () =>
-		location.reload(),
-	);
+if (!PRODUCTION) new EventSource("/esbuild").addEventListener("change", () => location.reload());
 
 // favicon can't be referenced from index.html, so it's imported and injected here
 const link = document.createElement("link");
@@ -28,7 +25,8 @@ class Sprite extends l.EngineObject3D {
 		this.pixelated = true;
 	}
 	update() {
-		this.pos3D.y = 2 + sin((time + this.phase) * 2) * 0.5; // bob up and down
+		// l.time is a live binding that changes every frame; read it at use time
+		this.pos3D.y = 2 + sin((l.time + this.phase) * 2) * 0.5; // bob up and down
 	}
 }
 
@@ -36,13 +34,9 @@ class Sprite extends l.EngineObject3D {
 
 function gameInit() {
 	new l.Render3DPlugin();
-	render3D.setSky(
-		hsl(0.65, 0.5, 0.15),
-		hsl(0.8, 0.4, 0.3),
-		hsl(0.65, 0.4, 0.1),
-	);
-	render3D.setFog(15, 40);
-	render3D.ambientColor = hsl(0.6, 0.2, 0.5);
+	l.render3D.setSky(hsl(0.65, 0.5, 0.15), hsl(0.8, 0.4, 0.3), hsl(0.65, 0.4, 0.1));
+	l.render3D.setFog(15, 40);
+	l.render3D.ambientColor = hsl(0.6, 0.2, 0.5);
 	new l.CameraControl3D(vec3(), 15, 0.5, 0.003);
 	new l.EngineObject3D(vec3(), l.buildGrid(vec2(30), 1, hsl(0.8, 0.2, 0.3))); // floor
 	new l.EngineObject3D(vec3(0, 2, 0), l.buildBox(2).setColor(hsl(0, 0, 0.7))); // cube
@@ -50,23 +44,12 @@ function gameInit() {
 		let i = 12;
 		i--; // ring of sprites from the tile sheet
 	)
-		new Sprite(
-			vec3(7, 2).rotateY((i / 12) * 2 * PI),
-			tile(i % 4, 16),
-			hsl(i / 12, 0.8, 0.7),
-		);
+		new Sprite(vec3(7, 2).rotateY((i / 12) * 2 * PI), tile(i % 4, 16), hsl(i / 12, 0.8, 0.7));
 }
 
-function gameUpdate() {} // input and game state
-function gameUpdatePost() {} // camera
-function gameRender() {} // background effects
+function gameUpdate() {}
+function gameUpdatePost() {}
+function gameRender() {}
+function gameRenderPost() {}
 
-function gameRenderPost() {
-	drawTile(vec2(0, 0), vec2(8), tile(3, 128));
-	drawText("LittleJS", vec2(0, 6), 2);
-}
-
-l.engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, [
-	tilesURL,
-]);
-
+l.engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, [tilesURL]);
